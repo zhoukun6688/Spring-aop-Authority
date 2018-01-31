60f905b23e45f73ee5262569a1c2fb5ad407ef7c
zhoukun6688 <18333658323@163.com>
2018-01-31 11:46:42 +0800

    删除异常

--- src/main/java/com/spring/mybatis/controller/LoginController.java
+++ src/main/java/com/spring/mybatis/controller/LoginController.java
@@ -25,7 +25,7 @@ public class LoginController {
 	
 	//登陆页面
 		@RequestMapping("/login")
-		public String login(Model model)throws Exception{
+		public String login(Model model){
 			return "login";
 		}
 		
